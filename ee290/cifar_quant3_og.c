d3ef4df04f857c88c3ba0067521a948e073b2ff8
Ryan Lund <ryan.lund@berkeley.edu>
2020-05-08 22:05:27 -0700

    Add more status checks to tests, remove time tracking

--- ee290/cifar_quant3_og.c
+++ ee290/cifar_quant3_og.c
@@ -50,6 +50,7 @@ int main (int argc, char * argv[]) {
     }
 
     #ifdef ELEM_T_IS_BFLOAT
+    printf("Starting bFloat Conversion\n");
     images_to_bfloat(4*num_batches, images, images_float);
     weights_to_bfloat(128, 64, conv_1_w, conv_1_w_float);
     weights_to_bfloat(192, 64, conv_2_w, conv_2_w_float);
@@ -57,9 +58,6 @@ int main (int argc, char * argv[]) {
     weights_to_bfloat(128, 128, fc_4_w, fc_4_w_float);
     weights_to_bfloat(64, 128, fc_5_w, fc_5_w_float);
     #endif
-
-    uint64_t start, end;
-    uint64_t im2col_cycles = 0, matmul_cycles = 0, pool_cycles = 0, conv_dw_cycles = 0, res_add_cycles = 0, other_cycles = 0;
 
     int correct = 0;
     int match = 0;
@@ -72,6 +70,7 @@ int main (int argc, char * argv[]) {
     int batch_labels[4];
     int batch_tf_labels[4];
 
+    printf("Starting Classification\n");
     for (int global_batch = 0; global_batch < num_batches; global_batch++) {
         gemmini_flush(0);
         // conv_1
@@ -88,69 +87,37 @@ int main (int argc, char * argv[]) {
             batch_labels[a] = labels[(4*global_batch) + a];
             batch_tf_labels[a] = tf_labels[(4*global_batch) + a];           
         }
-
-
-        start = read_cycles();
         
         im2col(conv_1_params.batch_size, conv_1_params.in_channels, conv_1_params.in_dim,
             conv_1_params.I, conv_1_params.K,
             batch_images, conv_1_in, &conv_1_params);
         
-        end = read_cycles();
-        im2col_cycles += end - start;
-
-        start = read_cycles();
-
         tiled_matmul_nn_auto(conv_1_params.I, conv_1_params.J, conv_1_params.K,
             conv_1_in, conv_1_w, NULL, conv_1_out,
             RELU, conv_1_params.output_scale, true, conv_1_params.output_scale,
             tiled_matmul_type, check, "conv_1");
 
-        end = read_cycles();
-        matmul_cycles += end - start;
-
-        start = read_cycles();
-
         pool_with_col2im(conv_1_params.I, conv_1_params.J,
             conv_1_params.batch_size, conv_1_params.out_channels, conv_1_params.out_dim_pooled,
             conv_1_out, conv_1_out_pooled, &conv_1_params);
 
-        end = read_cycles();
-        pool_cycles += end - start;
-
         // conv_2
-        start = read_cycles();
         
         im2col(conv_2_params.batch_size, conv_2_params.in_channels, conv_2_params.in_dim,
             conv_2_params.I, conv_2_params.K,
             conv_1_out_pooled, conv_2_in, &conv_2_params);
         
-        end = read_cycles();
-        im2col_cycles += end - start;
-
-        start = read_cycles();
-
         tiled_matmul_nn_auto(conv_2_params.I, conv_2_params.J, conv_2_params.K,
             conv_2_in, conv_2_w, NULL, conv_2_out,
             RELU, conv_2_params.output_scale, true, conv_2_params.output_scale,
             tiled_matmul_type, check, "conv_2");
 
-        end = read_cycles();
-        matmul_cycles += end - start;
-
-        start = read_cycles();
-
         pool_with_col2im(conv_2_params.I, conv_2_params.J,
             conv_2_params.batch_size, conv_2_params.out_channels, conv_2_params.out_dim_pooled,
             conv_2_out, conv_2_out_pooled, &conv_2_params);
 
-        end = read_cycles();
-        pool_cycles += end - start;
-
         // Convert conv output to fc input
         static elem_t fc_3_in[448][64] row_align(1);
-
-        start = read_cycles();
 
         for (size_t batch = 0; batch < conv_2_params.batch_size; batch++) {
             size_t pixel = 0;
@@ -164,41 +131,27 @@ int main (int argc, char * argv[]) {
             }
         }
 
-        end = read_cycles();
-        other_cycles += end - start;
-
         // fc_3
-        start = read_cycles();
 
         tiled_matmul_nn_auto(fc_3_params.I, fc_3_params.J, fc_3_params.K,
             fc_3_w, fc_3_in, NULL, fc_3_out,
             RELU, fc_3_params.output_scale, false, fc_3_params.output_scale,
             tiled_matmul_type, check, "fc_3");
 
-        end = read_cycles();
-        matmul_cycles += end - start;
 
         // fc_4
-        start = read_cycles();
 
         tiled_matmul_nn_auto(fc_4_params.I, fc_4_params.J, fc_4_params.K,
             fc_4_w, fc_3_out, NULL, fc_4_out,
             RELU, fc_4_params.output_scale, false, fc_4_params.output_scale,
             tiled_matmul_type, check, "fc_4");
 
-        end = read_cycles();
-        matmul_cycles += end - start;
-
         // fc_5
-        start = read_cycles();
 
         tiled_matmul_nn_auto(fc_5_params.I, fc_5_params.J, fc_5_params.K,
             fc_5_w, fc_4_out, NULL, fc_5_out,
             NO_ACTIVATION, fc_5_params.output_scale, false, fc_5_params.output_scale,
             tiled_matmul_type, check, "fc_5");
-
-        end = read_cycles();
-        matmul_cycles += end - start;
 
         // Find highest probs
         char * classes[] = {"plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"};
@@ -239,16 +192,6 @@ int main (int argc, char * argv[]) {
             }
         }
     }
-    
-    uint64_t total_cycles = im2col_cycles + matmul_cycles + pool_cycles + conv_dw_cycles + res_add_cycles + other_cycles;
-
-    printf("\nTotal cycles: %llu\n", total_cycles);
-    printf("Matmul cycles: %llu\n", matmul_cycles);
-    printf("Im2col cycles: %llu\n", im2col_cycles);
-    printf("Pooling cycles: %llu\n", pool_cycles);
-    printf("Depthwise convolution cycles: %llu\n", conv_dw_cycles);
-    printf("Other cycles: %llu\n", other_cycles);
-
 
     printf("Test Finished \n");
     printf("Correct: %d / %d \n", correct, 4*num_batches);
